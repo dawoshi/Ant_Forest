59d76e2839a5b1e2028e8733b40462abc945bc35
guoxinian <406853323@qq.com>
2020-02-13 18:13:52 +0800

    version1.0

--- README.md
+++ README.md
@@ -1,52 +1,62 @@
 # Ant_Forest  
 蚂蚁森林自动收能量，支持自动解锁和自动触发  
 目前支持收自己的、收好友能量  
-![image](https://github.com/1061700625/Ant_Forest/blob/master/pic/10.png)   
 
 ## QQ群
-913182235 
+406853323
+https://github.com/dawoshi/Ant_Forest
   
-（不知道是不是只有我这样，蚂蚁森林好友列表的界面布局变了，因此程序定位不到元素，导致卡在好友列表界面无响应；话说支付宝的动作这么迅速的吗）  
 
-！！！好了，针对上面的问题，本次版本中已修复。  
-由于基于控件已无法收取好友能量，因此修改为通过图片比对然后click，因此需要小手图片和安卓7及以上(大家都是9以上了吧)，文件都已传到网盘。  
   
 0、效果演示：  
-https://github.com/1061700625/Ant_Forest/blob/master/%E6%95%88%E6%9E%9C%E6%BC%94%E7%A4%BA.avi  
-![image](https://github.com/1061700625/Ant_Forest/blob/master/%E8%A7%86%E9%A2%91%E6%BC%94%E7%A4%BA.gif)  
 
 1、脚本实现功能
-启动后，脚本处于监听状态，注意开启保护，不要被后台关闭了。一般能量每天都是同一个时间点成熟，成熟后会通知栏发出通知(下面有说怎么在蚂蚁森林里开启通知)，脚本监听到该通知后，就会自动触发启动，点亮屏幕，解锁，进入蚂蚁森林。这时候，就需要用到你设置的时间了。因为是提前一分钟进入，所以能量还没成熟，但脚本会一直戳屏，直到到你设置的结束时间。所以输入时间并启动后，虽然没有任何动静，但只要不报错，一般就可以了；
+   a、可以自己设置时间间隔，>50000s。
+   b、程序根据时间间隔自动唤醒，解锁，
+      启动支付宝，
+      跳转到蚂蚁森林，
+      并收取自己和好友的能量，
+      等待下次时间间隔重复上述过程。
+   c、音量上键可停止脚本。
+   d、大致过程如日志所示：
+   ![image](https://github.com/dawoshi/Ant_Forest/blob/master/pic/111.png)
+   
 
 2、下载源码和autojs app
 
+   a、auto.js(Auto.js_v4.1.1.apk)
+      链接: https://pan.baidu.com/s/1EWFWjO8tZhmS0VGFlk2htA 提取码: 6ye3
+
+   b、程序代码
+      https://github.com/dawoshi/Ant_Forest/blob/master/
+
 2.5 、解压小手图片并放到自己能记住的路径下，等会要填到程序里的findImg()！！  
-![image](https://github.com/1061700625/Ant_Forest/blob/master/pic/11.png)
+![image](https://github.com/dawoshi/Ant_Forest/blob/master/pic/11.png)
 
 3、解压并导入到app
 解压源码后，放到手机存储下的“脚本”目录；也可以是其他的，记得就行  
-![image](https://github.com/1061700625/Ant_Forest/blob/master/pic/4.png)   
+![image](https://github.com/dawoshi/Ant_Forest/blob/master/pic/4.png)   
 打开autojs app，点击导入  
-![image](https://github.com/1061700625/Ant_Forest/blob/master/pic/5.png)  
+![image](https://github.com/dawoshi/Ant_Forest/blob/master/pic/5.png)  
 
 4、打开蚂蚁森林能量提醒功能  
 进入蚂蚁森林，点右上角“...”，进入设置，开启提醒  
-![image](https://github.com/1061700625/Ant_Forest/blob/master/pic/1.png)
-![image](https://github.com/1061700625/Ant_Forest/blob/master/pic/2.png)
+![image](https://github.com/dawoshi/Ant_Forest/blob/master/pic/1.png)
+![image](https://github.com/dawoshi/Ant_Forest/blob/master/pic/2.png)
  
 5、打开无障碍服务、通知权限  
-![image](https://github.com/1061700625/Ant_Forest/blob/master/pic/7.png)  
+![image](https://github.com/dawoshi/Ant_Forest/blob/master/pic/7.png)  
 点击后如有跳转，就对应的完成设置  
-![image](https://github.com/1061700625/Ant_Forest/blob/master/pic/8.png)  
+![image](https://github.com/dawoshi/Ant_Forest/blob/master/pic/8.png)  
 
 6、将autojs加入电池白名单、保护锁定，防止被后台关闭（这个大家都会的吧）  
 
 7、初次使用，程序中main()中的gesture()函数，是解锁功能，需要修改成自己的锁屏手势。  
 如果不改呢，就只是不能解锁而已，其他没影响  
-![image](https://github.com/1061700625/Ant_Forest/blob/master/pic/3.png) 
+![image](https://github.com/dawoshi/Ant_Forest/blob/master/pic/3.png) 
 
 可通过打开开发者选项中的指针位置查看坐标。  
-![image](https://github.com/1061700625/Ant_Forest/blob/master/pic/9.png)
+![image](https://github.com/dawoshi/Ant_Forest/blob/master/pic/9.png)
 
 8、运行脚本即可
 
@@ -57,12 +67,24 @@ https://github.com/1061700625/Ant_Forest/blob/master/%E6%95%88%E6%9E%9C%E6%BC%94
 
 
 
-10、如果想测试解锁，在这里加条语句  
+10、如果想测试解锁，可以单独测试这个函数  
 ```
-sleep(2000);  
-main();  
+function main1(){
+  log("唤醒设备");
+  device.wakeUp();
+  sleep(1000);
+  swipe(500,1800,500,500,100);
+  log("swipe over");
+  sleep(1000);
+  gesture(1000, [540,1425], [250,1700],[560,1700],[50,20],[80,10]);
+  log("gesture over");
+  sleep(1000);
+  log("唤醒完毕");
+  log("start antForest");
+  StartAll();
+  //ClickClose();
+}
 ```
-![image](https://github.com/1061700625/Ant_Forest/blob/master/pic/6.png)
 
 然后按照点击运行、输入时间、锁屏，静待手机自动解锁
 
@@ -76,4 +98,4 @@ main();
 更新下支付宝
 
 欢迎评论留言
-github：https://github.com/1061700625/Ant_Forest
+github：https://github.com/dawoshi/Ant_Forest
---
+++ ant_selects.js
@@ -0,0 +1,251 @@
+var MODE=0;
+
+
+function CloseApp() {
+    let packageName = currentPackage();
+    app.openAppSetting(packageName);
+    text(app.getAppName(packageName)).waitFor();
+    let is_sure = textMatches(/(.*强.*|.*停.*|.*结.*|.*行.*)/).findOne();
+    if (is_sure.enabled()) {
+        textMatches(/(.*强.*|.*停.*|.*结.*|.*行.*)/).findOne().click();
+        textMatches(/(.*确.*|.*定.*)/).findOne().click();
+        log(app.getAppName(packageName) + "应用已被关闭");
+        sleep(1000);
+        back();
+    } else {
+        log(app.getAppName(packageName) + "应用不能被正常关闭或不在后台运行");
+        back();
+    }
+}
+
+const EnterAntForest=function(){
+    app.startActivity({
+        action: "VIEW",
+	data: "alipays://platformapi/startapp?appId=60000002"
+    });
+    sleep(1000)
+}
+
+function StartAlipay()
+{
+    toast("启动支付宝")
+    app.launchApp("支付宝");
+    toast("启动完成");
+    sleep(1000);
+}
+
+function FindImg()
+{
+    //toast("截图中...");
+    var img = captureScreen();
+    while(!img)
+    {
+        sleep(100);
+        img = captureScreen();
+    }
+    toast("截图完成!");
+    var icon = images.read("icon.jpg");
+    var p = findImage(img, icon);
+    if(p){
+        toast("找到啦:" + p);
+    }else{
+        toast("没找到");
+    }
+    return p;
+}
+
+// 关闭提醒弹窗
+const ClearPopup = function() {
+    // 合种/添加快捷方式提醒
+    threads.start(function() {
+      let popup = idEndsWith("J_pop_treedialog_close").findOne(2000);
+      if (popup) popup.click();
+    });
+    // 活动
+    threads.start(function() {
+      let popup = descEndsWith("关闭蒙层").findOne(2000);
+      if (popup) popup.click();
+    }); 
+  }
+  const ClickClose = function () {
+    if (descEndsWith('关闭').exists()) {
+      descEndsWith('关闭')
+        .findOne(1000)
+        .click()
+    } else if (textEndsWith('关闭').exists()) {
+      textEndsWith('关闭')
+        .findOne(1000)
+        .click()
+    }
+  }
+
+function FriendsPower(){
+    var cnt = 0;
+    do {
+        var powerList = className("android.widget.Button").textStartsWith("收集能量").find();
+        powerList.forEach(function(item){
+            press(item.bounds().centerX(), item.bounds().centerY(), 80);
+            //item.click();
+            toast("收取一次");
+            sleep(200);
+        });
+        if(cnt++>=10) break;
+    } while (powerList.length);
+    toast("未成熟");
+    sleep(500);
+    //ClickClose();
+}
+
+//&&className("android.view.View").textEndsWith("排行榜").exists()
+
+function Friends()
+{
+    log("摘取取好友能量开始");
+    className("android.view.View").text("查看更多好友").findOne().click();
+    sleep(1000);
+    toast("收集好友能量");
+    var skip_cnt = 0;
+    while((!className("android.view.View").text("邀请").exists()) 
+    && (skip_cnt<=8))
+    {
+        if(className("android.view.View").text("查看更多好友").exists()){
+            className("android.view.View").text("查看更多好友").findOne().click();
+        }
+        if(id("tab_description").className("android.widget.TextView").text("首页").exists()){
+            EnterAntForest();
+        }
+        var p = FindImg();
+        sleep(500);
+        if(p)
+        {
+            toast("找到");
+            click(p.x-500, p.y+50);
+            var cnt = 0;
+            while(cnt<5)
+            {
+                if(className("android.widget.Button").text("浇水").exists())
+                {
+                    FriendsPower();
+                    back();
+                    sleep(1000);
+                    break;
+                }
+                else
+                {
+                    cnt ++;
+                    sleep(500);
+                }
+            }
+            skip_cnt = 0;
+            }
+        else
+        {
+            toast("上滑");
+            swipe(500,1800,500,500,500);
+            skip_cnt += 1;
+            if(skip_cnt > 8)
+            {
+                toast("结束退出！");
+                log("搜取好友能量结束");
+                ClickClose();
+            }
+        }
+    }
+    log("搜取好友能量结束");
+}
+
+ // 收取自己的能量
+ const myPowers = function(automator) {
+    log("搜取自己的能量");
+    if (descEndsWith("克").exists()) {
+      descEndsWith("克").untilFind().forEach(function(ball) {
+        automator.clickCenter(ball);
+        sleep(500);
+      });
+    } else if (textEndsWith("克").exists()) {
+      textEndsWith("克").untilFind().forEach(function(ball) {
+        automator.clickCenter(ball);
+        sleep(500);
+      });
+    }
+    log("搜取自己能量结束");
+  }
+
+function My(automator){
+    if(id("tab_description").className("android.widget.TextView").text("首页").exists()){
+        EnterAntForest();
+    }
+    myPowers(automator);
+}
+
+function StartAll(){
+    
+    StartAlipay();
+    EnterAntForest();
+    log("enterAntForest ok");
+    var dates = new Date();
+    if((dates.getHours()<9 &&dates.getHours()>7)||
+        (dates.getHours()>16&&dates.getHours()<18)){
+        var automator = require("./Automator.js");
+        My(automator);
+        sleep(1000);
+    }
+    Friends();
+    sleep(1000);
+    ClickClose();
+}
+
+function main1(){
+  log("唤醒设备");
+  device.wakeUp();
+  sleep(1000);
+  swipe(500,1800,500,500,100);
+  log("swipe over");
+  sleep(1000);
+  gesture(1000, [540,1425], [250,1700],[560,1700],[540,2020],[840,1700]);
+  log("gesture over");
+  sleep(1000);
+  log("唤醒完毕");
+  log("start antForest");
+  StartAll();
+  //ClickClose();
+}
+
+//程序从这里开始
+auto();
+alert("需开启无障碍服务、通知栏权限，音量上键可中止脚本");
+alert("注意！请核对'小手'图片存放位置，可在findImg()函数中修改，如果图片跟脚本是同一个目录，就不用改。默认: icon.jpg");
+
+console.show();
+var interval = 500000; 
+interval = console.rawInput("请输入摘取间隔时间(s), 如50000:");
+console.log(">> 间隔时间: "+interval);
+sleep(1000);
+console.hide();
+toast("Ready Go .....");
+sleep(1000)
+log("检查截图功能");
+if(!requestScreenCapture()){
+    toast("请求截图失败");
+    log("请求截图失败");
+    exit();
+} else {
+    log("截图功能检查成功");
+}
+
+setInterval(main1,interval);
+
+threads.start(function(){
+    //在子线程中调用observeKey()从而使按键事件处理在子线程执行
+    events.observeKey();
+    events.on("key_down", function(keyCode, events){
+        //音量键关闭脚本
+        if(keyCode == keys.volume_down){
+           ClickClose();
+           exit();
+        }
+    });
+});
+events.on("exit", function(){
+    toast("脚本已结束");
+});
